bb90d7c57ba9d8b811830cbae5fac10357b36407
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-04-11 14:45:09 +0300

    update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import Nav from './Nav';
 import About from './About';
 import Shop from './Shop';
+import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
    <Router>
       <div>
         <Nav/>
-        <Route path="/about" component={About}/>
-        <Route path="/shop" component={Shop}/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/shop" component={Shop}/>
+        </Switch>
+        
     </div>
    </Router>
   );
--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,23 @@
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function Nav() {
+    const navStyle={
+        color:"white"
+    }
   return (
     <div>
         <nav>
           <h3>Logo</h3>
-          <ul className='nav-links'>
-                <li>About</li>
-                <li>Shop</li>
+            <ul className='nav-links'>
+                <Link style={navStyle} to="/about">
+                    <li>About</li>
+                </Link>
+                <Link style={navStyle} to="/shop">
+                    <li>Shop</li>
+                </Link>    
             </ul>
-          </nav>
+        </nav>
     </div>
   );
 }
