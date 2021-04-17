dcb257a59621bf004a67a35213272512ccaae3c1
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-04-17 23:11:31 +0300

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -8,7 +8,9 @@ function Nav() {
   return (
     <div>
         <nav>
+        <Link style={navStyle} to="/">
           <h3>Logo</h3>
+        </Link>
             <ul className='nav-links'>
                 <Link style={navStyle} to="/about">
                     <li>About</li>
