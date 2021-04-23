4b4f26989e14fa2c9d0b58c9f1d359ec105eb4b4
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-04-23 11:50:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Nav from './Nav';
 import About from './About';
 import Shop from './Shop';
 import Home from './Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, useParams } from 'react-router-dom';
 import { firestore } from './firebase';
 function App() {
   const db=firestore.collection("users").add({name:"alperen"});
@@ -15,10 +15,22 @@ function App() {
           <Route path="/" exact component={Home}/>
           <Route path="/about" component={About}/>
           <Route path="/shop" component={Shop}/>
+          <Route path="/:id" children={<Child />} />
         </Switch>
-        
     </div>
    </Router>
+  );
+}
+
+function Child() {
+  // We can use the `useParams` hook here to access
+  // the dynamic pieces of the URL.
+  let { id } = useParams();
+
+  return (
+    <div>
+      <h3>ID: {id}</h3>
+    </div>
   );
 }
 
