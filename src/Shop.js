547458fa8a515c12ce4946b08476b2972a20ea1f
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-05-08 23:29:18 +0300

    Update Shop.js

--- src/Shop.js
+++ src/Shop.js
@@ -5,8 +5,7 @@ export default class Shop extends React.Component {
     return(
       <div>
         <h1>Shop</h1>
-        {/* <h2>{this.props.match.params.id}</h2>
-        <h3>{this.props.dataFromParent}</h3> */}
+        
       </div>
     )
   }
