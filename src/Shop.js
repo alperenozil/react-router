7cbf054a6aba8375828e51096af5fc930001dc79
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-05-09 22:50:43 +0300

    Update Shop.js

--- src/Shop.js
+++ src/Shop.js
@@ -1,15 +1,40 @@
 import './App.css';
 import React,{useState,useEffect} from 'react';
 import { useParams } from 'react-router';
+import { firestore } from './firebase';
+
 function Shop() {
-  const { id } = useParams();
+  const [blogs,setBlogs]=useState([])
+  const { noteid } = useParams();
+  fetchBlogs(noteid);
+  useEffect(() => {  
+    
+  }, [])
     return(
       <div>
         <h1>Shop</h1>
-        <h2>{id}</h2>
+        <h2>{noteid}</h2>
+        <div className="App">
+          {
+            blogs && blogs.map(blog=>{
+              return(
+                <h3>{blog.note}</h3>
+              )
+            })
+          }
+        </div>
       </div>
     )
 
+}
+export async function fetchBlogs(name){
+  console.log(name)
+  const response=firestore.collection(useParams());
+  const data=await response.get();
+  const posts=data.docs.map(item=>{
+    return {id: item.id, ...item.data()}
+  })
+  setBlogs([...posts])
 }
 
 export default Shop;
